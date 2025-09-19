225dce9372806bd9bb7da443456e172728d75629
D3vT001$ <pro17820@outlook.com>
2025-09-19 15:57:03 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -6,20 +6,22 @@ mkdir -p $HOME/.vnc
 echo "vncpass" | vncpasswd -f > $HOME/.vnc/passwd
 chmod 600 $HOME/.vnc/passwd
 
-# Kill any old VNC
+# Kill any existing VNC session
 vncserver -kill :1 || true
 rm -rf /tmp/.X1-lock /tmp/.X11-unix/X1
 
-# Start Openbox session in background
-openbox-session &
-
-# Start VNC server
+# Start VNC server first
 vncserver :1 -geometry 1280x800 -depth 24
 
-# Launch Chromium + File Manager inside DISPLAY :1
+# Export DISPLAY so apps know where to draw
 export DISPLAY=:1
+
+# Start Openbox session
+openbox-session &
+
+# Launch Chromium + File Manager
 chromium-browser --no-sandbox --disable-dev-shm-usage &  
 pcmanfm &
 
-# Start noVNC
+# Start noVNC proxy
 exec /usr/share/novnc/utils/launch.sh --vnc localhost:5901 --listen 6080
