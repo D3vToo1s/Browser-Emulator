a0663a5d7cecfb4a62d21c14f62876e361878a3a
D3vT001$ <pro17820@outlook.com>
2025-09-19 15:54:25 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,15 +1,25 @@
 #!/usr/bin/env bash
 set -e
 
-# Start Openbox session
+# Setup VNC password
+mkdir -p $HOME/.vnc
+echo "vncpass" | vncpasswd -f > $HOME/.vnc/passwd
+chmod 600 $HOME/.vnc/passwd
+
+# Kill any old VNC
+vncserver -kill :1 || true
+rm -rf /tmp/.X1-lock /tmp/.X11-unix/X1
+
+# Start Openbox session in background
 openbox-session &
 
-# Start VNC server on :1
+# Start VNC server
 vncserver :1 -geometry 1280x800 -depth 24
 
-# Launch Chromium and file manager inside VNC session
-DISPLAY=:1 chromium-browser --no-sandbox & 
-DISPLAY=:1 pcmanfm &
+# Launch Chromium + File Manager inside DISPLAY :1
+export DISPLAY=:1
+chromium-browser --no-sandbox --disable-dev-shm-usage &  
+pcmanfm &
 
-# Start noVNC (websockify on port 6080)
-websockify --web=/usr/share/novnc/ 6080 localhost:5901
+# Start noVNC
+exec /usr/share/novnc/utils/launch.sh --vnc localhost:5901 --listen 6080
